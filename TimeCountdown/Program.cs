cb96235fa2e9b29adc98209d39229c40275b91af
Александр Жаворонок <C0baltt@live.com>
2021-04-29 12:40:58 +0300

    start development

---
+++ TimeCountdown/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TimeCountdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date1 = new DateTime();
+            Console.WriteLine(date1); // 01.01.0001 0:00:00
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TimeCountdown/СountdownDay.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TimeCountdown
+{
+    class СountdownDay
+    {
+        private DayOfWeek _nextDayLearning;
+        private DateTime _timeStartOfTraining;
+        //.AddHours(double 19)
+
+        internal DateTime TimeStartOfTraining
+        {
+            get
+            {
+                return _timeStartOfTraining;
+            }
+            set
+            {
+                _timeStartOfTraining.AddHours(19);
+            }
+        }
+
+        private DayOfWeek NextDayLearning { get => _nextDayLearning; set => _nextDayLearning = FindNextDayLearning(); }
+
+        public СountdownDay()
+        {
+            //_nextDayLearning = FindNextDayLearning();
+            NextDayLearning = _nextDayLearning;
+        }
+
+        private DayOfWeek FindNextDayLearning()
+        {
+            if ((DateTime.Now.DayOfWeek != DayOfWeek.Monday) &&
+                (DateTime.Now.DayOfWeek <= DayOfWeek.Wednesday))
+            {
+                return DayOfWeek.Wednesday;
+            }
+            else
+            {
+                return DayOfWeek.Monday;
+            }
+        }
+
+        //date1.Subtract(date2)
+    }
+}
